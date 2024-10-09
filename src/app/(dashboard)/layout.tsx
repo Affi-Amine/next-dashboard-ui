334434681b2664eb7b96e474141b0fafa71aa54b
mac <mac@MacBook-Air-de-mac.local>
2024-10-09 14:16:03 +0100

    Setting up the menu section

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import Menu from "../components/Menu";
+import Navbar from "../components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -12,11 +13,14 @@ export default function DashboardLayout({
     <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14% p-4">
       <Link href="/" className="flex justify-center items-center gap-2 lg:justify-start">
         <Image src='/logo.png' alt="logo" width={32} height={32} />
-        <span className="hidden lg:block">SchooLama</span>
+        <span className="hidden lg:block">HyperSchool</span>
       </Link>
       <Menu />
     </div>
     {/* RIGHT */}
-    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]"></div>
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#f7f8fa] overflow-scroll">
+      <Navbar />
+      {children}
+    </div>
   </div>
 }
