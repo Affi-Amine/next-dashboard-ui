a2afd48ad6b4b5a36fbec402c4a72f49b379e186
mac <mac@MacBook-Air-de-mac.local>
2024-10-17 14:52:52 +0100

    Admin dashboard done

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,11 +1,15 @@
-import AttendanceChart from '@/components/AttendanceChart'
-import CountChart from '@/components/CountChart'
-import UserCard from '@/components/UserCard'
-import React from 'react'
+import Announcements from "@/app/components/Announcements";
+import EventCalendar from "@/app/components/EventCaldendar";
+import FinanceChart from "@/app/components/FinanceChart";
+import AttendanceChart from "@/components/AttendanceChart";
+import CountChart from "@/components/CountChart";
+
+import UserCard from "@/components/UserCard";
+
 
 const AdminPage = () => {
   return (
-    <div className='p-4 flex gap-4 flex-col md:flex-row'>
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* LEFT */}
       <div className="w-full lg:w-2/3 flex flex-col gap-8">
         {/* USER CARDS */}
@@ -25,17 +29,19 @@ const AdminPage = () => {
           <div className="w-full lg:w-2/3 h-[450px]">
             <AttendanceChart />
           </div>
-          {/* BOTTOM CHART */}
-          <div className="">
-
-          </div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]">
+          <FinanceChart />
         </div>
       </div>
-
       {/* RIGHT */}
-      <div className="w-full lg:w-1/3">b</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+        <EventCalendar />
+        <Announcements />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminPage
+export default AdminPage;
